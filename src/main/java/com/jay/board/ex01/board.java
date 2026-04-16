58f1f068182b748caf2a2087e2ed927d0a37cc1e
coding-faster <berichmore@naver.com>
2026-04-16 11:45:43 +0900

    java crud-7강

--- src/main/java/com/jay/board/ex01/board.java
+++ src/main/java/com/jay/board/ex01/board.java
@@ -12,18 +12,29 @@ import java.util.Scanner;
 public class board {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("== 자바 게시판 시작 ==");
+        int lastId = 0;
 
+        System.out.println("== 자바 게시판 시작 == ");
 
         while(true) {
             System.out.print("명령 ");
             String cmd = sc.nextLine();
 
-            if (cmd.equals("exit")) {
+            if(cmd.equals("/user/article/write")) {
+                System.out.println("== 게시물 작성 ==");
+                System.out.println("제목을 입력하세요: ");
+                String title = sc.nextLine();
+
+                System.out.println("내용을 입력하세요: ");
+                String content = sc.nextLine();
+                int id = ++lastId;
+                System.out.printf("%d번 게시물이 등록되었습니다.", id);
+            } else if(cmd.equals("exit")) {
                 System.out.println("프로그램을 종료합니다.");
                 break;
+            } else{
+                System.out.println(" ㅇㅇㅇ");
             }
-            System.out.println("입력받은 명령어 " + cmd);
         }
     }
 }
